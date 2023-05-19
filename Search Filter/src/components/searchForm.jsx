e7b06cc0243b0069d0b58eb8b4b7d2a40c583cdd
2p\halshorbagy <hazem.frontend@gmail.com>
2023-05-19 04:27:26 +0300

    filterlist-static

---
+++ Search Filter/src/components/searchForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import ButtonAction from "./ui/Button";
+import InputImplement from "./ui/Input";
+import ListItemSingle from "./ui/listItem";
+const ItemsList = [
+  { id: "1", name: "Joe", user_type: "Developer" },
+  { id: "2", name: "Hill", user_type: "Designer" },
+  { id: "3", name: "hazem", user_type: "ios" },
+  { id: "4", name: "ahmed", user_type: "android" },
+  { id: "5", name: "test", user_type: "devops" },
+  { id: "6", name: "tarek", user_type: "Engineer" },
+];
+function SearchForm() {
+  const [serachKeyItem, setSearchKey] = useState("");
+  const [filterList, setFilterList] = useState(ItemsList);
+
+  const UsersInfo = filterList.map((singleItem) => {
+    return <ListItemSingle item={singleItem} />;
+  });
+
+  SearchForm = () => {
+    if (serachKeyItem.length > 0) {
+      setFilterList(
+        ItemsList.filter(
+          (filteredList) =>
+            filteredList.name.toLowerCase() == serachKeyItem ||
+            filteredList.user_type.toLowerCase() == serachKeyItem
+        )
+      );
+    } else {
+      setFilterList(ItemsList);
+    }
+  };
+  let onChange = (event) => {
+    setSearchKey(event.target.value);
+  };
+  return (
+    <>
+      <div className="search-Form d-flex m-auto mt-3 mb-3">
+        <InputImplement onChangeInput={onChange} />
+        <ButtonAction title={"Search"} action={SearchForm} />
+      </div>
+      <div className="table-padding">
+        <table>
+          <thead>
+            <tr>
+              <th>#Id</th>
+              <th>Name</th>
+              <th>User Type</th>
+            </tr>
+          </thead>
+          <tbody>{UsersInfo}</tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default SearchForm;
